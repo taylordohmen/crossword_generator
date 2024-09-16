d621d6606e1ef5759e5c3f120a0df8db45fa4c5c
Taylor Dohmen <taylor.dohmen@colorado.edu>
2024-09-16 14:43:51 -0600

    numbering

--- grid.lua
+++ grid.lua
@@ -19,15 +19,15 @@ local too_central = function(x, y)
     return math.abs(x - (d + 1 - x)) < 3 and math.abs(y - (d + 1 - y)) < 3
 end
 
-local unavailable = function (x, y)
-    return  taken(x, y) or not in_bounds(x, y)
+local available = function (x, y)
+    return not taken(x, y) and in_bounds(x, y)
 end
 
 local forbidden = function(x, y)
-    local east = {unavailable(x + 1, y), unavailable(x + 2, y), unavailable(x + 3, y)}
-    local north = {unavailable(x, y + 1), unavailable(x, y + 2), unavailable(x, y + 3)}
-    local west = {unavailable(x - 1, y), unavailable(x - 2, y), unavailable(x - 3, y)}
-    local south = {unavailable(x, y - 1), unavailable(x, y - 2), unavailable(x, y - 3)}
+    local east = {not available(x + 1, y), not available(x + 2, y), not available(x + 3, y)}
+    local north = {not available(x, y + 1), not available(x, y + 2), not available(x, y + 3)}
+    local west = {not available(x - 1, y), not available(x - 2, y), not available(x - 3, y)}
+    local south = {not available(x, y - 1), not available(x, y - 2), not available(x, y - 3)}
 
     if (east[1] or east[2] or east[3]) and (west[1] or west[2] or west[3]) then
         return true
@@ -52,24 +52,63 @@ local forbidden = function(x, y)
     return false
 end
 
+local stringify = function(coords)
+    local str_coordinates = ""
+
+    for i = 1, #coords do
+        local x, y = table.unpack(coords[i])
+        str_coordinates = str_coordinates .. x .. "/" .. y .. ", "
+    end
+
+    str_coordinates = string.sub(str_coordinates, 1, -3)
+    return str_coordinates
+end
+
+local save_to_file = function(name, coord_list)
+    local file = io.open(name, "w")
+
+    for i = 1, #coord_list do
+        local x, y = table.unpack(coord_list[i])
+        file:write(x .. "," .. y .. "\n")
+    end
+    
+    file:close()
+end
+
 function black_coordinates(dimension)
     d = dimension
 
     for i = 1, d * d / 12 do
         local x, y = math.random(d),  math.random(d)
+
         while taken(x, y) or forbidden(x, y) or forbidden(d + 1 - x, d + 1 - y) or too_central(x, y) do
             x, y = math.random(d), math.random(d)
         end
+
         table.insert(coordinates, {x, y})
         table.insert(coordinates, {d + 1 - x, d + 1 - y})
     end
 
-    local str_coordinates = ""
-    for i = 1, #coordinates do
-        local x, y = table.unpack(coordinates[i])
-        str_coordinates = str_coordinates .. x .. "/" .. y .. ", "
-    end
-    str_coordinates = string.sub(str_coordinates, 1, -3)
+    save_to_file("black_coordinates.txt", coordinates)
 
+    local str_coordinates = stringify(coordinates)
+    tex.sprint(str_coordinates)
+end
+
+
+function numbers()
+    local spots = {}
+
+    for y = 1, d do
+        for x = 1, d do
+            if available(x, y) and (not available(x - 1, y) or not available(x, y - 1)) then
+                table.insert(spots, {x, y})
+            end
+        end
+    end
+
+    save_to_file("number_coordinates.txt", spots)
+
+    local str_coordinates = stringify(spots)
     tex.sprint(str_coordinates)
 end
